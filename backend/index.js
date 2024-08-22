bf51b8911417991495d6f4dd4d600a9eaca522ec
Tesfamariam777 <tafessemekete@gmail.com>
2024-08-22 06:29:35 -0700

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
@@ -9,7 +8,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,10 +19,6 @@ app.get('/generate-token', (req, res) => {
     const token = jwt.sign(payload,process.env.JWT_SECRET, {expiresIn: '1hr'});
     res.json({token});
 })
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', index.html));
-})
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

f4aeea33fedbf7027c445e06030a96bce5b72e7c
Tesfamariam777 <tafessemekete@gmail.com>
2024-08-22 06:33:01 -0700

    updated

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,5 @@
 const config = {
-    API_URL: 'http://localhost:5000'
+    // API_URL: 'http://localhost:5000'
+    API_URL: 'https://poc-nodejs-react-auto-login-server-j8ke5hl7c.vercel.app'
 }
 export default config;
